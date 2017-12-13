94cbdf2338ff14d303a16f5b2708c1d19d745ae7
cassianomaia <cassmala@gmail.com>
2017-12-13 15:55:02 -0200

    Iniciando o problema 2

---
+++ Trabalho 2 - Arvores Geradoras Mínimas com Prim/prim.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import networkx as nx
+import pylab as plt
+
+def ExtractMin(Q):
+	dict_min = {}
+
+	for key, value in Q.items():
+		dict_min[key] = value['key']
+
+	return min(dict_min, key=dict_min.get)
+
+def PrimMST(G, r):
+	λ = {}
+	π = {}
+	for v in G.nodes():
+		λ[v] = np.inf
+		π[v] = 'null'
+
+	λ[r] = 0
+	S = {}
+
+	while λ:
+		u = ExtractMin(λ)
+		del λ[u]
+		S.append(u)
+		
+		for neighboor in G[u]:
+			if neighboor in λ:
+				if G[u][neighboor]['weight'] < λ[neighboor]:
+					π[neighboor] = u
+					λ[neighboor] = G[u][neighboor]['weight']
+
