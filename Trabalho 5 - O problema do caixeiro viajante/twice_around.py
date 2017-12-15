290f8dc631e6e37710d218505ac6de5fd4c89179
Cassiano Maia <cassmala@gmail.com>
2017-12-15 15:12:28 -0200

    Errors and errors

--- Trabalho 5 - O problema do caixeiro viajante/twice_around.py
+++ Trabalho 5 - O problema do caixeiro viajante/twice_around.py
@@ -7,29 +7,40 @@ from heapq import heappop, heappush
 pop = heappop
 push = heappush
 
+def tour_weight(G): 
+    weight = 0
+    weight_vector = nx.get_edge_attributes(G, 'weight')
+    for v in G.edges(): 
+        weight += weight_vector[v]
+    return weight
+
 def twice_around(G, s):
-    Mst = nx.minimum_spanning_tree(G) # gero a mst a partir do grafo original
-    Mst = nx.MultiGraph(Mst) # como somente multigrafos aceitam arestas paralelas
+    Mst = nx.minimum_spanning_tree(G)
+    Mst = nx.MultiGraph(Mst)
        
     for u,v in Mst.edges():
-        Mst.add_edge(u,v)     #duplico arestas da mst
+        Mst.add_edge(u,v)
     
-    tour_euler = list(nx.eulerian_circuit(Mst, s)) # gero um circuito euleriano
-    Resultado = nx.Graph()
+    tour_euler = list(nx.eulerian_circuit(Mst, s)) 
+    Result = nx.Graph()
     aux = []
     for u,v in tour_euler: 
         aux.append(u)
         aux.append(v)
     h = []
     for i in aux: 
-        if (i not in h):    # elimino repetições
+        if (i not in h):
             h.append(i)
     h.append(s)
     for i in range (30):
-        Resultado.add_edge(h[i],h[i+1]) # gero grafo resultante
-        Resultado[h[i]][h[i+1]]['weight'] = G[h[i]][h[i+1]]['weight'] # copiando também o peso    
-    return Resultado
+        Result.add_edge(h[i],h[i+1])
+        Result[h[i]][h[i+1]]['weight'] = G[h[i]][h[i+1]]['weight']   
+    return Result
 
 
 A = np.loadtxt('ha30_dist.txt')
 G = nx.from_numpy_matrix(A)
+
+Result = twice_around(G, 1)
+nx.draw_networkx(Result)
+plt.show()
